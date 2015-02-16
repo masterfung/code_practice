a1393055f15f26cff47826ffc837b07769a46931
Tsung Hung <hungmasterj@gmail.com>
2015-02-16 14:40:15 -0800

    expanded the table to have three columns and started a new db

--- db.py
+++ db.py
@@ -1,17 +1,14 @@
 import sqlite3
 
-db = sqlite3.connect('Awesome DB')
+db = sqlite3.connect('people')
 
 c = db.cursor()
 
-table = ('people')
-
-
-c.execute('''CREATE TABLE IF NOT EXISTS {}
-            (Name Text)'''.format(table))
+c.execute('''CREATE TABLE people
+            (first Text, last Text, age FLOAT)''')
 
 c.execute(
-	"insert into people values ('Jennifer')"
+	"insert into people values ('Jennifer', 'Hua', 31)"
 	)
 
 c.execute('select * from people')
