2c29e3e38f01ee70ee7713daf7b2fd08b4c7b3f2
Tsung Hung <hungmasterj@gmail.com>
2015-02-17 07:57:51 -0800

    zipping functions

--- sequence_fun.py
+++ sequence_fun.py
@@ -43,3 +43,15 @@ k = {'red':22, 'blue':77, 'green': 133, 'alpha': 60}
 
 colors(**k)
 
+
+first = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+second = [10, 293, 10, 29, 39, 493, 29, 93, 92]
+third = [0, 29, 392, 10, 93, 3, 4, 0, 9]
+
+for item in zip(first, second, third):
+	print(item)
+
+total_three = [first, second, third]
+total_end = list(zip(*total_three))
+
+print(total_end)
